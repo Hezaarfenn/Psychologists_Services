6ab19434ca3d9d4138c25fd539a912832ad64265
Heza GÖLCÜK <h.golcuk91@outlook.com>
2025-06-05 10:22:19 +0300

    finished

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,8 +1,11 @@
 import { RingLoader } from "react-spinners";
+
 const Loader = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <RingLoader color="[rgb(var(--primary-color))]" size={150} />;
+      <div className="transform -translate-y-50">
+        <RingLoader color="rgb(var(--primary-color))" size={150} />
+      </div>
     </div>
   );
 };
